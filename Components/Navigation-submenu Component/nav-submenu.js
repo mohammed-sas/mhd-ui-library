9bfcec9e2cd7cc5f53b7bb7d410987b0ae158b73
mohammed-sas <89216938+mohammed-sas@users.noreply.github.com>
2022-01-31 19:26:17 +0530

    fix : bug in dropdown

--- Components/Navigation-submenu Component/nav-submenu.js
+++ Components/Navigation-submenu Component/nav-submenu.js
@@ -1,17 +1,19 @@
 const subMenuBtns = document.querySelectorAll('.submenu-links>li');
 const subMenuLists = document.querySelectorAll('.submenu-items');
 const subMenuLinks = document.querySelectorAll('.submenu-links');
+let isOpen=false;
 subMenuBtns.forEach(btn=>{
-    btn.addEventListener('mouseover',()=>{
+    btn.addEventListener('click',()=>{
+        
         subMenuBtns.forEach(item=>{
             item.childNodes[1].style.display='none';
         })
-        btn.childNodes[1].style.display='flex'
-    })
-    btn.addEventListener('click',()=>{
-        subMenuLists.forEach(item=>{
-            item.style.display='none';
-        })
+        if(!isOpen){
+            btn.childNodes[1].style.display='flex';
+        }else{
+            btn.childNodes[1].style.display='none';
+        }
+        isOpen=!isOpen;
     })
 
 })
